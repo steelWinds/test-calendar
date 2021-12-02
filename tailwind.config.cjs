251f4a2c6c218b106bf72e9f5aed75105f3d9e60
ninja <kirillsurov0@gmail.com>
2021-12-02 14:39:41 +0900

    update(docs): add screens props in tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
     theme: {
         screens: {
             lg: '1024px',
-            xl: '1280px'
+            xl: '1280px',
+            '2xl': '1520px'
         }
     },
     purge: [
